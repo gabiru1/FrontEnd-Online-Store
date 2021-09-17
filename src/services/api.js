38e8a34d65dbaf953259c273a199babdd605eea4
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-09-17 16:50:30 -0300

    requisito 1 terminado

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 export async function getCategories() {
-  // Implemente aqui
-  const request = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
-  return request;
+  const categories = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  return categories.json();
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const categories = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  return categories.json();
 }
